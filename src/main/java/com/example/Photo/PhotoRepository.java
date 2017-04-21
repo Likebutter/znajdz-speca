0fabf286463a6a83a757a0c4a4145bd6d48cd660
Aleksander Kubala <aleksander.kubala@student.wat.edu.pl>
2017-04-21 23:54:53 +0200

    Dodawanie jobów - multipart + aws cloud

--- src/main/java/com/example/Photo/PhotoRepository.java
+++ src/main/java/com/example/Photo/PhotoRepository.java
@@ -6,12 +6,13 @@ import com.example.Job.Job;
 import com.example.Opinion.Opinion;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PhotoRepository extends CrudRepository<Photo, Integer>{
 
-    List<Photo> findAllByJob(Job job);
+    List<Photo> findAllByJob(@Param(value = "Job") Job job);
     List<Photo> findAllByOpinion(Opinion opinion);
     List<Photo> findAllByCompany(Company company);
 }
